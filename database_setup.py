1011430e6400fd60f32d2535298e57a0a812bcc2
вячеслав <vyacheslav@MacBook-Pro-vaceslav.local>
2020-11-18 03:29:30 +0300

    New biuld 4

--- database_setup.py
+++ database_setup.py
@@ -20,8 +20,36 @@ class Movie(Base):
     id = Column(Integer, primary_key=True)
     title = Column(String(250), nullable=False)
     author = Column(String(250), nullable=False)
-    cast = Column(String(250))
+    genre = Column(String(250))
     price = Column(String(250))
+
+
+class Branch(Base):
+    __tablename__ = 'branch'
+    id = Column(Integer, primary_key=True)
+    name = Column(String(250), nullable=False)
+    seats = Column(Integer, nullable=False)
+
+class Screening(Base):
+    __tablename__ = 'screening'
+    id = Column(Integer, primary_key=True)
+    movie_id = Column(Integer, ForeignKey(Movie.id), nullable=False)
+    branch_id = Column(Integer, ForeignKey(Branch.id), nullable=False)
+    screening_time = Column(String(250), nullable=False)
+
+
+class Seat(Base):
+    __tablename__ = 'seat'
+    id = Column(Integer, primary_key=True)
+    row = Column(Integer, nullable=False)
+    number = Column(Integer, nullable=False)
+    branch_id = Column(Integer, nullable=False)
+
+class User(Base):
+    __tablename__ = "user"
+    id = Column(Integer, primary_key=True)
+    username = Column(Integer, nullable=False)
+    password = Column(Integer, nullable=False)
 
 engine = create_engine('sqlite:///books-collection.db')
 Base.metadata.create_all(engine)
